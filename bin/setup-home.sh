540d53611589107dd712f97842b4307e3d66412e
Isobel Redelmeier <iredelmeier@gmail.com>
2020-08-14 12:50:24 -0700

    Buncha changes

---
+++ bin/setup-home.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+set -eu
+
+TARGET="${TARGET:-"$HOME"}"
+
+rm -rf /tmp/foo
+mkdir -p /tmp/foo
+
+cd "$(realpath "$(dirname "${BASH_SOURCE[0]}")")/../home"
+
+find . -type f -print0 | while IFS= read -rd $'\0' path; do
+  path="$(echo "$path" | sed -e "s/^\.\/\{,\}//g")" # trim off the leading "."
+  dir="$(dirname -- "$path")"
+
+  if [[ "$dir" != "." ]]; then
+    mkdir -p "${TARGET}/.${dir}"
+  fi
+
+  ln -sfn "${PWD}/${path}" "${TARGET}/.${path}"
+done
---
+++ home/local/share/konsole/tomorrow night.profile
@@ -0,0 +1,17 @@
+[Appearance]
+AntiAliasFonts=true
+ColorScheme=tomorrow night
+Font=DejaVu Sans Mono,14,-1,5,50,0,0,0,0,0
+UseFontLineChararacters=false
+
+[General]
+Name=tomorrow night
+Parent=FALLBACK/
+
+[Interaction Options]
+MiddleClickPasteMode=1
+MouseWheelZoomEnabled=false
+
+[Scrolling]
+HistoryMode=2
+ScrollBarPosition=2
